31d5c01788326e45a385213c68bca041434eb97d
lailson <lailson.pi@gmail.com>
2019-06-11 14:57:07 -0300

    componentes

---
+++ 05-Componentes/componente-intro/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <h1>Contador</h1>
+    <app-contador/>
+    <app-contador/>
+    <app-contador/>
+    <app-contador/>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style>
+
+</style>
