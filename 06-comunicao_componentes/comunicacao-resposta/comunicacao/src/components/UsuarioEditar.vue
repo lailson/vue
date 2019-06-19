17c26773431a761be0fe3558f0e0ecd104199382
lailson <lailson.pi@gmail.com>
2019-06-19 16:06:58 -0300

    comunicacao

---
+++ 06-comunicao_componentes/comunicacao-resposta/comunicacao/src/components/UsuarioEditar.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="componente">
+        <h2>Alterar os Dados de Usuário</h2>
+        <p>Edite as informações</p>
+        <p>Nome do usuário: <strong>{{nome}}</strong></p>
+        <button @click="reiniciarNome" >Reinicia Nome</button>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['nome'],
+    methods: {
+        reiniciarNome(){
+            this.nome= "lailson"
+            this.$emit('nomeMudou', this.nome)
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .componente {
+        flex: 1;
+        background-color: #98b99a;
+        color: #fff;
+    }
+</style>
